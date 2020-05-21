11d9fce47b1c096835914911c8873abc3cf788d4
Hamza Imran <hamzaimran@Hamzas-Mac.local>
2020-05-22 00:17:28 +0500

    add notifications page

---
+++ Smart Worker/View/NotificatiosCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificatiosCell.swift
+//  Smart Worker
+//
+//  Created by Hamza Imran on 21/05/2020.
+//  Copyright © 2020 Hamza Imran. All rights reserved.
+//
+
+import UIKit
+
+class NotificatiosCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
